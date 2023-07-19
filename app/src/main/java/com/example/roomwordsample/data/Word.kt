20f1898f3f350af169ec2eaf73946f6e892a66a9
vaneIv <appvane@gmail.com>
2023-07-19 16:33:20 +0200

    We created Entity class Word and WordDao interface.

---
+++ app/src/main/java/com/example/roomwordsample/data/Word.kt
@@ -0,0 +1,11 @@
+package com.example.roomwordsample.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+data class Word(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo val word: String
+)
